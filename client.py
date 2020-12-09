92e901fad9fac9a870fe438a681441000a9a2291
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-12-09 15:14:00 -0800

    Added Robert's "client.py" & "server.py"

--- client.py
+++ client.py
@@ -1,1 +1,91 @@
-""" Add your Client class from your TCP assignment here """
+#!/usr/bin/env python3
+import socket
+import pickle
+
+# Robert's Client from TCP Project
+class Client(object):
+    def __init__(self):
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = None
+        self.port = None
+        self.name = None
+        self.clientid = 0
+        self.menu = None
+
+    def get_client_id(self):
+        return self.clientid
+
+    # get new client info and server specs
+    def getClientInfo(self, host, port):
+        self.host = input("Enter the server IP Address: ")
+        if self.host == "":
+            self.host = host
+        self.port = input("Enter the server port: ")
+        if self.port == "":
+            self.port = port
+        self.name = input("Your id key (i.e your name): ")
+        if self.name == "":
+            self.name = "anonymous"
+
+    # connect to the server
+    def connect(self, host="127.0.0.1", port=13000):
+        self.getClientInfo(host, port)
+        self.clientSocket.connect((host, port))  # connect is done here
+        print("\nSuccessfully connected to server at {host}/{port}".format(host=host, port=port))
+
+        # handshake between client and server
+        # client gets its client_id
+        self.clientid = self.receive()['clientid']
+
+        # send the name of the client to the server
+        self.send(self.name)
+
+        # log some info for the client
+        print("Your client info is:")
+        print("Client Name: {name}".format(name=self.name))
+        print("Client ID: {id}".format(id=self.clientid))
+
+    # send data from client to CH
+    def send(self, data):
+        serialized_data = pickle.dumps(data)
+        self.clientSocket.send(serialized_data)\
+
+    # receive data from CH
+    def receive(self, MAX_BUFFER_SIZE=8192):
+        data_from_client = self.clientSocket.recv(MAX_BUFFER_SIZE)
+        data = pickle.loads(data_from_client)
+        return data
+
+    # close the client
+    def close(self):
+        self.clientSocket.close()
+
+    def run(self):
+        receiveData = self.receive()
+
+        # get the file
+        newFile = open(receiveData['file_name'], 'wb')
+        newFile.write(receiveData['file_content'])
+
+        # get the menu object
+        receiveData = self.receive()
+        self.menu = receiveData['menu']
+        self.menu.set_client(self)
+
+        # communicate
+        while True:
+            self.menu.show_menu()
+            self.menu.process_user_data()
+
+
+# if __name__ == '__main__':
+#     try:
+#         client = Client()
+#         client.connect()
+#         client.run()
+#     except KeyboardInterrupt as err:
+#         print('\n(x) You left abruptly')
+#     except Exception as err:
+#         print('\n(x) Client Error --> {err}'.format(err=err))
+#     finally:
+#         exit()
--- server.py
+++ server.py
@@ -1,1 +1,67 @@
-""" Add your Server class from your TCP assignment here """
+from builtins import object
+import socket
+from threading import Thread
+
+# Robert's Server from TCP Project
+class Server(object):
+    MAX_NUM_CONN = 10
+
+    def __init__(self, ip_address='127.0.0.1', port=13000):
+        # save ip & host
+        self.host = ip_address
+        self.port = port
+
+        # create an INET, STREAMing socket
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # server state
+        self.clients = {}  # key: client_id, value: clientHandler
+        self.names = {}  # key: client_id, value: client_name
+        self.rooms = {}  # key: room_number, value: [client_id]
+
+    def _bind(self):
+        self.serversocket.bind((self.host, self.port))
+
+    def _listen(self):
+        self.serversocket.listen(self.MAX_NUM_CONN)
+        print("Server listening at {host}/{port}".format(host=self.host, port=self.port))
+
+    def _accept_clients(self):
+        while True:
+            # accept the new client
+            clienthandler, addr = self.serversocket.accept()
+
+            # start a new thread
+            Thread(target=self.client_handler_thread, args=(clienthandler, addr), daemon=True).start()
+
+    # main thread entry point
+    def client_handler_thread(self, clientsocket, address):
+        # create the client handler
+        # client_handler = ClientHandler(self, clientsocket, address)
+
+        # # init the CH
+        # client_handler.init()
+
+        try:
+            # run the main logic
+            client_handler.run()
+        except EOFError as err:
+            client_id = client_handler.client_id
+            print("(x) Client Handler Thread Error --> Client ({name}:{client_id}) left abruptly".format(name=self.names[client_id], client_id=client_id))
+            client_handler.delete_client_data()
+        except Exception as err:
+            print("(x) Client Handler Thread Error --> {err}".format(err=err))
+            client_handler.delete_client_data()
+
+    # main server logic
+    def run(self):
+        self._bind()
+        self._listen()
+        self._accept_clients()
+
+# if __name__ == '__main__':
+#     try:
+#         server = Server()
+#         server.run()
+#     except Exception as err:
+#         print("(x) Server Error --> {err}".format(err=err))
